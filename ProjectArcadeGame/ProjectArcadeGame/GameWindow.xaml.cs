0ae2e48e269db1ee1ab3543895fbf319d3fba19d
MartenYeager <marten.ter.meer@gmail.com>
2021-10-19 15:00:18 +0200

    Continuation of P2
    -Added "Main Menu" button to GameWindow
    -Added "Reset" button to GameWindow (currently has no logic)
    -Added MoveLeft and MoveRight logic for Player2
    
    Know issue: Application is unable to focus on Gamecanvas and Gamecanvas2 at the same time.

--- ProjectArcadeGame/ProjectArcadeGame/GameWindow.xaml.cs
+++ ProjectArcadeGame/ProjectArcadeGame/GameWindow.xaml.cs
@@ -31,7 +31,6 @@ namespace ProjectArcadeGame
         /*Player2*/
         private bool MoveLeft2 = false, MoveRight2 = false, MoveUp2 = false, MoveDown2 = false, Jumping2 = false;
         int Force2 = 10;
-        int Speed2 = 8;
         int JumpSpeed2 = 10;
         private DispatcherTimer GameTimer = new DispatcherTimer();
 
@@ -98,20 +97,33 @@ namespace ProjectArcadeGame
             GameTimer.Start();
 
             GameCanvas.Focus();
+            GameCanvas2.Focus();
 
+            //Player1
             PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario.png"));
             Player.Fill = PlayerSkin;
             GroundSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario_ground1.png"));
             Ground.Fill = GroundSkin;
-           
+
+            //Player2
+            //PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario.png"));
+            //Player2.Fill = PlayerSkin;
+            GroundSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario_ground1.png"));
+            Ground2.Fill = GroundSkin;
+
         }
-        #region Player1
+
         private void GameEngine(object sender, EventArgs press)
         {
+            //Player1
             double TopPos = Canvas.GetTop(Player);
             double LeftPos = Canvas.GetLeft(Player);
             double GroundTop = Canvas.GetTop(Ground);
+            //Player2
+            double LeftPos2 = Canvas.GetLeft(Player2);
 
+            Rect PlayerHitBox ;
+            Rect PlatformHitBox ;
             foreach (var x in GameCanvas.Children.OfType<Rectangle>())
             {
                 if (x.Tag != null)
@@ -119,27 +131,43 @@ namespace ProjectArcadeGame
                     var Tag = (string)x.Tag;
                     if (Tag == "Platform")
                     {
-                        Rect PlayerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
-                        Rect PlatformHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+                        PlayerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
+                        PlatformHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
                     }
                 }
             }
 
+            //Player1 MoveLeft
             if (MoveLeft && LeftPos > 5)
             {
                 Canvas.SetLeft(Player, Canvas.GetLeft(Player) - Speed);
                 PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/Mario_left.png"));
                 Player.Fill = PlayerSkin;
             }
+            //Player2 MoveLeft
+            if (MoveLeft2 && LeftPos2 > 5)
+            {
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) - Speed);
+                //PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/Mario_left.png"));
+                //Player.Fill = PlayerSkin;
+            }
 
+            //Player1 MoveRight
             if (MoveRight)
             {
                 Canvas.SetLeft(Player, Canvas.GetLeft(Player) + Speed);
                 PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario.png"));
                 Player.Fill = PlayerSkin;
-
             }
-            
+            //Player2 MoveRight
+            if (MoveRight2)
+            {
+                Canvas.SetLeft(Player2, Canvas.GetLeft(Player2) + Speed);
+                //PlayerSkin.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Characters/mario.png"));
+                //Player.Fill = PlayerSkin;
+            }
+
+            //Player1
             if (MoveUp && Jumping == false && Force > 0)
             {
                 Canvas.SetTop(Player, Canvas.GetTop(Player) - JumpSpeed);
@@ -147,6 +175,7 @@ namespace ProjectArcadeGame
                 Force -= 1;
             }
 
+            //Player1
             if (Force < 1)
             {
                 Jumping = true;
@@ -155,6 +184,11 @@ namespace ProjectArcadeGame
                 Force += 1;
             }
         }
-        #endregion 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Visibility = Visibility.Visible;
+            this.Close();
+        }
     }
 }
