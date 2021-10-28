cc355dc7ecc4ff00c332f66157a0ab552732c408
Honor <71923017+Honor-js@users.noreply.github.com>
2021-10-28 23:32:00 +0200

    Database change date format
    Format is now dd-MM-yyyy

---
+++ ProjectArcadeGame/UnitTestConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnitTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("6^7=" + TotDeMacht(6, 7));
+        }
+        public static int TotDeMacht(int grondtal, int macht)
+        {
+            return (int)Math.Pow(grondtal, macht);
+        }
+    }
+
+}
+   
